145e2fa34c0377722d3918278b85ba7becbb0a32
xswatx <xswatx@naver.com>
2019-05-03 17:17:01 +0900

    다했지만 아직 오류가 남아있음.

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,6 +1,20 @@
 import React ,{Component} from 'react';//React 는 객체, Component 는 멤버(라이브러리)
 import {post} from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
 
+
+
+const styles = theme =>({
+    hidden:{
+        display:'none'
+    }
+});
 
 class CustomerAdd extends Component{
 
@@ -12,14 +26,25 @@ class CustomerAdd extends Component{
             birthday:'',
             gender:'',
             job:'',
-            fileName:''
+            fileName:'',
+            open:false
         }
     }
     handleFormSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         this.addCustomer()
         .then((response)=>{
-            console.log(response.data)
+            console.log(response.data);
+            this.props.stateRefresh();
+        })
+        this.setState({
+            file:null,
+            userName:'',
+            birthday:'',
+            gender:'',
+            job:'',
+            fileName:'',
+            open:false
         })
     }
 
@@ -39,7 +64,7 @@ class CustomerAdd extends Component{
         const url = '/api/customers';
         const formData = new FormData();
         formData.append('image', this.state.file);
-        formData.append('name', this.state.name);
+        formData.append('name', this.state.userName);
         formData.append('birthday', this.state.birthday);
         formData.append('gender', this.state.gender);
         formData.append('job', this.state.job);
@@ -51,20 +76,53 @@ class CustomerAdd extends Component{
         }
         return post(url, formData, config);
     }
+
+    handelClickOpen = () => {
+        this.setState({
+            open:true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file:null,
+            userName:'',
+            birthday:'',
+            gender:'',
+            job:'',
+            fileName:'',
+            open:false
+        })
+    }
+
+
     render(){
+        const {classes}=this.props;
         return(
-            //input 테그가 있으면 상태변화감지를 위해 onChange함수 handle~ 가 있는거임
-            <form onSubmit={this.handleFormSubmit}>
-            <h1>고객추가</h1>
-            프로필 이미지:<input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input><br/>
-            이름 : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></input><br/>
-            생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></input><br/>
-            성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}></input><br/>
-            직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></input><br/>
-            <button type="submit">추가하기</button>
-            </form>
-        )
+            <div>
+            <Button variant="contained" color="primary" onClick={this.handelClickOpen}>고객 추가하기</Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+            <DialogTitle>고객 추가</DialogTitle>
+            <DialogContent>
+                <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input><br/>
+                <label htmlFor="raised-button-file">
+                <Button variant="contained" color="primary" component="span" name="file">
+                    {this.state.fileName===''? "프로필 이미지 선택": this.state.fileName}
+                </Button>
+                </label><br></br>
+                <TextField label="이름" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></TextField>
+                <TextField label="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></TextField>
+                <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}></TextField>
+                <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></TextField>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+            </DialogActions>
+            </Dialog>
+            </div>
+        );
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const multer = require('multer');
+const upload = multer({dest: './upload'});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/image', express.static('./upload'));
 
 const data = fs.readFileSync('./database.json','utf8');
 const conf = JSON.parse(data);
@@ -19,12 +22,36 @@ const conn = mysql.createConnection({
 });
 conn.connect();
 
+app.post('/api/customers', upload.single('image'), (req, res)=>{
+  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?, now(), 0)';
+  let image = '/image/'+ req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  conn.query(sql, params, (err, rows, fields)=>{
+    res.send(rows);
+  })
+})
+
+app.delete('/api/customers/:id', (req,res)=>{
+  let sql ='update customer set isDeleted = 1 where id =?';
+  let params=[req.params.id];
+  conn.query(sql, params, (err, rows, field)=>{
+    res.send(rows);
+  })
+})
+
 app.get('/api/customers', (req, res)=>{
-    conn.query('select * from customer',(err, rows, fields) => {
+    conn.query('select * from customer where isDeleted = 0',(err, rows, fields) => {
         res.send(rows);
       }
     );
 });
+
+
+
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));
